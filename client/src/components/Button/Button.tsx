f6cdea438de75263b6b86e5a7289b982a7ce87b4
r1fel <felix.reiter@gmx.de>
2023-10-23 17:27:04 +0200

    created Button componenent

---
+++ client/src/components/Button/Button.tsx
@@ -0,0 +1,53 @@
+import classNames from 'classnames';
+import './Button.scss';
+
+interface ButtonProps {
+  children: React.ReactNode;
+  primary?: boolean;
+  secondary?: boolean;
+  success?: boolean;
+  warning?: boolean;
+  fail?: boolean;
+  ghost?: boolean;
+  rounded?: boolean;
+  className?: string;
+}
+
+function Button({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  fail,
+  ghost,
+  rounded,
+  ...rest
+}: ButtonProps) {
+  const classes = classNames(rest.className, 'btn', {
+    'btn-primary': primary,
+    'btn-second': secondary,
+    'btn-success': success,
+    'btn-warning': warning,
+    'btn-fail': fail,
+    'btn-rounded': rounded,
+    'btn-ghost': ghost,
+    'text-blue-500': primary && ghost,
+    'text-cyan-500': secondary && ghost,
+    'text-green-500': success && ghost,
+    'text-yellow-500': warning && ghost,
+    'text-red-500': fail && ghost,
+  });
+
+  //className makes sure the classes are correctly written. rest.className is important to make sure, that this object doesn't overwrite the custom classNames, passed in by props. Like this they get added to the object before being overwritten.
+
+  return (
+    <button {...rest} className={classes}>
+      {children}
+    </button>
+  );
+}
+
+//with {...rest} we pass down all the other props (like different event handlers) directly down to the underlying button component
+
+export default Button;
