ce6b2990fa5fd9b69a48df8ac1f86b47ef274392
r1fel <felix.reiter@gmx.de>
2023-10-25 19:08:41 +0200

    fixed formatting of api files

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,26 +1,26 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const passport = require("passport");
-const users = require("../controllers/users");
-const catchAsync = require("../utils/catchAsync");
-const { storeReturnTo, isLoggedIn } = require("../middleware");
+const passport = require('passport');
+const users = require('../controllers/users');
+const catchAsync = require('../utils/catchAsync');
+const {storeReturnTo, isLoggedIn} = require('../middleware');
 
-router.route("/register").post(catchAsync(users.register));
+router.route('/register').post(catchAsync(users.register));
 
-router.route("/login").post(
+router.route('/login').post(
   storeReturnTo,
-  passport.authenticate("local", {
+  passport.authenticate('local', {
     //TODO FR: use failureFlash with tostify?
     failureFlash: true,
     // failureRedirect: '/login'
   }),
-  catchAsync(users.login)
+  catchAsync(users.login),
 );
 
 // client checks if user has ongoing session
-router.route("/auth").get(isLoggedIn, catchAsync(users.sendIsAuthenticated));
+router.route('/auth').get(isLoggedIn, catchAsync(users.sendIsAuthenticated));
 
 //TODO ER: check how passport destroys the session
-router.get("/logout", users.logout);
+router.get('/logout', users.logout);
 
 module.exports = router;
