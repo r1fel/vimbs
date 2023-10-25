5ab7c457f146720b7a94d252c59774905b3dc858
esta3 <rauschert-e@web.de>
2023-10-25 14:56:37 +0200

    auth route - isLoggedIn sends false if not logged in, if logged in sendIsAuthenticated controller sends true

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,13 +1,13 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 //TODO ER: do we need registeredUser and dbUserentry?
 // react version: user register - to do: cookies/session
 module.exports.register = async (req, res, next) => {
   try {
-    const {email, username, password} = req.body;
-    const user = new User({email, username});
+    const { email, username, password } = req.body;
+    const user = new User({ email, username });
     const registeredUser = await User.register(user, password);
-    const dbUserentry = await User.find({username: username});
+    const dbUserentry = await User.find({ username: username });
     res.send(dbUserentry);
     req.login(registeredUser, (err) => {
       if (err) return next(err);
@@ -23,12 +23,16 @@ module.exports.register = async (req, res, next) => {
 // react version:
 // Esther: get session and redirect logic set up
 module.exports.login = async (req, res) => {
-  const {username} = req.body;
-  const user = await User.find({username: username});
+  const { username } = req.body;
+  const user = await User.find({ username: username });
   // req.flash('success', `Welcome back ${username}!`);
   // const redirectUrl = res.locals.returnTo || '/books/mine';
   // res.redirect(redirectUrl);
   res.send(user);
+};
+
+module.exports.sendIsAuthenticated = async (req, res) => {
+  res.send(true);
 };
 
 //TODO ER check how logout works with passport
@@ -41,6 +45,6 @@ module.exports.logout = (req, res, next) => {
     // req.flash('success', 'Goodbye!');
     // res.redirect('/');
   });
-  res.send('successfully logged out on the BE!');
-  console.log('bye!');
+  res.send("successfully logged out on the BE!");
+  console.log("bye!");
 };
