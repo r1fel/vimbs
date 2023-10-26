7d95c347764318f2d79275a9be88b1420175fdfd
r1fel <felix.reiter@gmx.de>
2023-10-26 12:20:33 +0200

    logger fixed to log only in dev mode

--- client/src/util/log.tsx
+++ client/src/util/log.tsx
@@ -2,7 +2,12 @@ import getTime from './getTime';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function log(...args: any) {
-  console.log(getTime(), ...args);
+  if (import.meta.env.MODE !== 'production') {
+    console.log(getTime(), ...args);
+  } else {
+    //! in production log to file!
+    return;
+  }
 }
 
 export default log;
