722993036dacbb7f6b048ea1df854d0bcce27f20
r1fel <felix.reiter@gmx.de>
2023-10-29 13:47:55 +0100

    added navigation to cards

--- client/src/components/ItemCard/ItemCard.tsx
+++ client/src/components/ItemCard/ItemCard.tsx
@@ -1,17 +1,26 @@
 import {logger} from '../../util/logger';
 import shortenText from '../../util/shortenText';
 import './ItemCard.scss';
+import {Link} from 'react-router-dom';
 
-function ItemCard({itemName, itemDescription, itemImages}) {
+interface ItemCardProps {
+  itemName: string;
+  itemDescription: string;
+  itemImages: string;
+}
+
+function ItemCard({itemName, itemDescription, itemImages}: ItemCardProps) {
   const shortName = shortenText(15, itemName);
   const shortDescription = shortenText(70, itemDescription);
 
   return (
-    <div className="item-card">
-      <img className="item-card__img" src={itemImages}></img>
-      <h1 className="item-card__name">{shortName}</h1>
-      <p className="item-card__descr">{shortDescription}</p>
-    </div>
+    <Link className="item-card__link" to={'/auth'}>
+      <div className="item-card">
+        <img className="item-card__img" src={itemImages}></img>
+        <h1 className="item-card__name">{shortName}</h1>
+        <p className="item-card__descr">{shortDescription}</p>
+      </div>
+    </Link>
   );
 }
 
