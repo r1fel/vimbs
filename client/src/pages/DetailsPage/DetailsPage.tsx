35893b64f0308d5d96dc4d78641adaa53b7ed53f
r1fel <felix.reiter@gmx.de>
2023-11-11 10:04:57 +0100

    details page reveiving correct props

--- client/src/pages/DetailsPage/DetailsPage.tsx
+++ client/src/pages/DetailsPage/DetailsPage.tsx
@@ -4,7 +4,7 @@ import NoAuthRedirect from '../../components/NoAuthRedirect';
 import ItemDetails from '../../components/ItemDetails/ItemDetails';
 import Button from '../../components/Button/Button';
 import Chat from '../../components/Chat/Chat';
-import {initializeRequest} from '../../services/ItemServices';
+import NavBar from '../../components/NavBar/NavBar';
 
 function DetailsPage(): JSX.Element {
   // NoAuthRedirect();
@@ -13,6 +13,7 @@ function DetailsPage(): JSX.Element {
 
   return (
     <div>
+      <NavBar />
       <ItemDetails id={id} />
       <Chat />
       <Link to={`item/${id}/request`}>
