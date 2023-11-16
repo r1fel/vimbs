e0db160ae5543ea5ff147d6cec6747a31cec17e0
r1fel <felix.reiter@gmx.de>
2023-11-16 21:42:42 +0100

    no auth redirect new logic

--- client/src/features/authentication/components/NoAuthRedirect.tsx
+++ client/src/features/authentication/components/NoAuthRedirect.tsx
@@ -1,26 +1,57 @@
+import {useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {useQuery} from '@tanstack/react-query';
+import {useAtom} from 'jotai';
 import {checkAuthStatus} from '../services/AuthServices';
 import {logger} from '../../../util/logger';
-import {useNavigate} from 'react-router-dom';
-
-// import {useContext} from 'react';
-// import UserContext from '../context/UserContext';
+import {userDataAtom, isLoggedInAtom} from '../../../context/userAtoms';
 
 function NoAuthRedirect() {
   const navigate = useNavigate();
-  // const {userData} = useContext(UserContext);
+  const [userData] = useAtom(userDataAtom);
+  const [isLoggedIn] = useAtom(isLoggedInAtom);
 
-  const checkCurrentAuthStatus = async () => {
-    const currentAuthStatus = await checkAuthStatus();
-    if (currentAuthStatus === true) {
-      logger.log('auth status true');
-      return;
-    } else {
-      logger.log('auth status false, redirect now');
-      navigate('/auth');
-    }
-  };
+  const authQuery = useQuery({
+    queryKey: ['auth'],
+    queryFn: checkAuthStatus,
+  });
 
-  return checkCurrentAuthStatus();
+  useEffect(() => {
+    const handleAuthStatus = () => {
+      if (authQuery.isSuccess) {
+        logger.log(
+          'authQuery details are :',
+          authQuery.data,
+          'isLoggedIn',
+          isLoggedIn,
+          'userData:',
+          userData,
+        );
+        if (
+          authQuery.data &&
+          isLoggedIn &&
+          userData &&
+          window.location.pathname === '/auth'
+        ) {
+          logger.log('auth status true, going from auth back to the main page');
+          navigate('/');
+        } else if (authQuery.data && isLoggedIn && userData) {
+          logger.log('auth status true');
+          return;
+        } else {
+          logger.log('auth status false, redirect now');
+          navigate('/auth');
+        }
+      }
+
+      if (authQuery.isError) {
+        logger.error('could not fetch auth status:', authQuery.error);
+        navigate('/auth');
+      }
+    };
+
+    handleAuthStatus();
+  }, [authQuery, isLoggedIn, userData]);
 }
 
 export default NoAuthRedirect;
