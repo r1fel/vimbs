6687a083e0646fd3db3a37b56c74ffb52020a745
r1fel <felix.reiter@gmx.de>
2023-11-16 14:32:25 +0100

    adapted itemCards for delete

--- client/src/pages/MyItemsPage/MyItemsPage.tsx
+++ client/src/pages/MyItemsPage/MyItemsPage.tsx
@@ -3,13 +3,18 @@ import {useState} from 'react';
 import ItemList from '../../components/ItemList/ItemList';
 import {fetchItems} from '../../services/ItemServices';
 import NavBar from '../../components/NavBar/NavBar';
+import {useAtom} from 'jotai';
+import {isDeleteItemModalOpenAtom} from '../../context/itemAtoms';
+import Modal from '../../components/Modal/Modal';
 
 function MyItemsPage(): JSX.Element {
   const [x, setX] = useState('x');
+  const [isModalOpen, setIsModalOpen] = useAtom(isDeleteItemModalOpenAtom);
   // NoAuthRedirect();
   return (
     <div>
       <NavBar />
+      <Modal>HAllo</Modal>
       <ItemList url={'item/mine'} fetchFunction={fetchItems} trigger={x} />
     </div>
   );
