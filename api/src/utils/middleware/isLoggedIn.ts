e9dc6aaa895fd58afe055ee4ba707a5222a288a8
esta3 <rauschert-e@web.de>
2023-11-18 20:59:51 +0100

    add api console.logs to follow the google auth flow and change redirects to CLIENT_URL

--- api/src/utils/middleware/isLoggedIn.ts
+++ api/src/utils/middleware/isLoggedIn.ts
@@ -3,10 +3,17 @@ import { Request, Response, NextFunction } from 'express';
 
 const isLoggedIn = (req: Request, res: Response, next: NextFunction) => {
   if (!req.isAuthenticated()) {
-    // res.redirect('http://localhost:5173/login');
-    return res.send(false);
+    console.log(
+      'Checking authentication status not successful:',
+      req.isAuthenticated()
+    );
+    return res.redirect(`${process.env.CLIENT_URL}/auth`);
+    // return res.send(false);
   }
-  // console.log('isLoggedIn just ran');
+  console.log(
+    'Checking authentication status successful:',
+    req.isAuthenticated()
+  );
   return next();
 };
 
