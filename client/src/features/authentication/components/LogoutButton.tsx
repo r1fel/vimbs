2521a055976e7b98246094a9b70a50a6430d1d7b
r1fel <felix.reiter@gmx.de>
2023-11-18 12:50:59 +0100

    working logout button

--- client/src/features/authentication/components/LogoutButton.tsx
+++ client/src/features/authentication/components/LogoutButton.tsx
@@ -1,24 +1,34 @@
-import {useQuery} from '@tanstack/react-query';
-import {redirect, useNavigate} from 'react-router-dom';
+import {useMutation, QueryClient, useQueryClient} from '@tanstack/react-query';
+import {useNavigate} from 'react-router-dom';
 import {useAtom} from 'jotai';
+import '../../../components/NavBar/NavBar.scss';
 import {userDataAtom, isLoggedInAtom} from '../../../context/userAtoms';
 import {handleLogout} from '../services/AuthServices';
+import {IoLogOutOutline} from 'react-icons/io5';
+import {logger} from '../../../util/logger';
 
-function LogoutButton() {
+function LogoutButton({className}) {
   const [userData, setUserData] = useAtom(userDataAtom);
   const [isLoggedIn, setIsLoggedIn] = useAtom(isLoggedInAtom);
 
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
-  const authQuery = useQuery({
-    queryKey: ['auth', 'logout'],
-    queryFn: handleLogout,
-    onSucess: async () => {
-      setUserData('');
-      setIsLoggedIn(false);
+  const logoutMutation = useMutation({
+    mutationFn: handleLogout,
+    onSuccess: async () => {
+      // queryClient.setQueryData(['item', itemData.data[0]._id], itemData);
+      queryClient.invalidateQueries(['auth'], {exact: true});
+      await setUserData('');
+      await setIsLoggedIn(false);
+      logger.log('logout mutation successful');
       navigate('/auth');
     },
   });
+
+  return (
+    <IoLogOutOutline className={className} onClick={logoutMutation.mutate} />
+  );
 }
 
 export default LogoutButton;
