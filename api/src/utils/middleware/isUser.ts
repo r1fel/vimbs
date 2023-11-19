6bd7fb4ba9aa5ae6d6066f17f810963bc4aa63e9
esta3 <rauschert-e@web.de>
2023-11-19 21:59:48 +0100

    test isUser

--- api/src/utils/middleware/isUser.ts
+++ api/src/utils/middleware/isUser.ts
@@ -5,12 +5,12 @@ import mongoose from 'mongoose';
 import ExpressError from '../ExpressError';
 
 const isUser = (req: Request, res: Response, next: NextFunction) => {
-  if (!req.user) return new ExpressError('user is undefined', 500);
+  if (!req.user) return next(new ExpressError('user is undefined', 500));
   const currentUser = req.user._id;
   const reqUserId = new mongoose.Types.ObjectId(req.params.userId);
   if (!currentUser.equals(reqUserId))
     return next(
-      new ExpressError('You are not allowed to view this content!', 403)
+      new ExpressError('You are not allowed to view this content!', 403),
     );
 
   return next();
---
+++ api/tests/utils/middleware/isUser.test.ts
@@ -0,0 +1,65 @@
+import { Request, Response, NextFunction } from 'express';
+import mongoose from 'mongoose';
+import isUser from '../../../src/utils/middleware/isUser';
+import ExpressError from '../../../src/utils/ExpressError';
+
+jest.mock('../../../src/utils/ExpressError');
+
+describe('isUser middleware', () => {
+  let req: Partial<Request>;
+  let res: Partial<Response>;
+  let next: NextFunction;
+
+  beforeEach(() => {
+    req = {
+      user: { _id: new mongoose.Types.ObjectId('65553888ac436ca1c6654129') }, // Mock the user object with _id
+      params: {
+        userId: new mongoose.Types.ObjectId(
+          '65553888ac436ca1c6654129',
+        ).toString(),
+      }, // Mock the userId parameter
+    };
+    res = {};
+    next = jest.fn();
+  });
+
+  test('should call next if the requesting user is the same as the user in the path', () => {
+    // use both userIds to be as defined above
+
+    isUser(req as Request, res as Response, next);
+
+    // Check that next was called
+    expect(next).toHaveBeenCalled();
+    expect(next).not.toHaveBeenCalledWith(expect.any(ExpressError));
+    expect(ExpressError).not.toHaveBeenCalled();
+  });
+
+  test('should return an ExpressError with status 500 if req.user is undefined', () => {
+    // Set req.user to undefined
+    req.user = undefined;
+
+    isUser(req as Request, res as Response, next);
+
+    // Check that it returns an ExpressError with status 500
+    expect(next).toHaveBeenCalledWith(expect.any(ExpressError));
+    expect(ExpressError).toHaveBeenCalledWith('user is undefined', 500);
+  });
+
+  test('should return an ExpressError with status 403 if the requesting user is different from the user in the path', () => {
+    // Set req.params.userId to a different user ID
+    req.params = {
+      userId: new mongoose.Types.ObjectId(
+        '66553888ac436ca1c6654129',
+      ).toString(),
+    };
+
+    isUser(req as Request, res as Response, next);
+
+    // Check that it returns an ExpressError with status 403
+    expect(next).toHaveBeenCalledWith(expect.any(ExpressError));
+    expect(ExpressError).toHaveBeenCalledWith(
+      'You are not allowed to view this content!',
+      403,
+    );
+  });
+});
