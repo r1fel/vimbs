edc54430e8e790df803c0dd25e047ed12c16a898
r1fel <felix.reiter@gmx.de>
2023-11-21 15:10:56 +0100

    change isLoggedIn to work with frontend again

--- api/src/utils/middleware/isLoggedIn.ts
+++ api/src/utils/middleware/isLoggedIn.ts
@@ -3,17 +3,12 @@ import { Request, Response, NextFunction } from 'express';
 
 const isLoggedIn = (req: Request, res: Response, next: NextFunction) => {
   if (!req.isAuthenticated()) {
-    console.log(
-      'Checking authentication status not successful:',
-      req.isAuthenticated()
-    );
-    return res.redirect(`${process.env.CLIENT_URL}/auth`);
-    // return res.send(false);
+    console.log('Checking authentication status not successful:');
+    //! Change redirect to backend, but talk about it
+    // return res.redirect(`${process.env.CLIENT_URL}/auth`);
+    return res.send(false);
   }
-  console.log(
-    'Checking authentication status successful:',
-    req.isAuthenticated()
-  );
+  console.log('Checking authentication status successful:');
   return next();
 };
 
