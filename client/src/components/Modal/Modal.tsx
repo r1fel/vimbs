e257ff46a0ea5c1a6cc5db67e17621203b6924db
r1fel <felix.reiter@gmx.de>
2023-11-22 21:18:00 +0100

    built modal for delete confirmation

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -1,7 +1,21 @@
+import Button from '../Button/Button';
 import './Modal.scss';
 
-function Modal({ children }) {
-  return <div>{children}</div>;
+function Modal({ isOpen, setIsOpen, children }) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-delete-item">
+      {children}
+      <Button
+        onClick={() => {
+          setIsOpen(false);
+        }}
+      >
+        Close
+      </Button>
+    </div>
+  );
 }
 
 export default Modal;
