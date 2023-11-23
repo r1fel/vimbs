a1cf8b0b664be3fbcc31c250d2945419373be3d5
esta3 <rauschert-e@web.de>
2023-11-23 13:55:11 +0100

    adjust itemSchema to validate that at least one category is supplied in request for create/updateItem

--- api/src/routes/itemRoutes.ts
+++ api/src/routes/itemRoutes.ts
@@ -29,7 +29,7 @@ itemRoutes
   )
   .post(
     isLoggedIn,
-    // validateItem,
+    validateItem,
     //
     createItem,
   );
--- api/src/utils/middleware/schemas/itemSchema.ts
+++ api/src/utils/middleware/schemas/itemSchema.ts
@@ -1,18 +1,26 @@
 // Request Validation Schema, needed for Middleware: defines the accepted req.body data
 
 import Joi from 'joi';
-import { categoriesArray } from '../../../enums';
 
 const itemSchema = Joi.object({
   item: Joi.object({
     picture: Joi.string().allow(null, '').optional(),
     name: Joi.string().required(),
     description: Joi.string().allow(null, '').optional(),
-    categories: Joi.array()
-      .items(
-        Joi.string()
-          .valid(...categoriesArray)
-          .required(),
+    categories: Joi.object()
+      .pattern(
+        Joi.string().valid(
+          'HouseAndGarden',
+          'ChildAndBaby',
+          'MediaAndGames',
+          'AdultClothing',
+          'SportAndCamping',
+          'Technology',
+          'Other',
+        ),
+        Joi.object({
+          subcategories: Joi.array().min(1).items(Joi.string()).required(),
+        }),
       )
       .required(),
   }).required(),
