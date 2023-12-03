057305873e4fde98ad3c495678a343a94cc2c697
r1fel <felix.reiter@gmx.de>
2023-12-03 10:59:42 +0100

    adjusted modal to be TS proof

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -1,16 +1,24 @@
 import ReactDom from 'react-dom';
+import { ModalProps } from './ModalTypes';
 
 import './Modal.scss';
 
-function Modal({ isOpen, children }) {
+function Modal({ isOpen, children }: ModalProps) {
   if (!isOpen) return null;
+
+  const portalElement = document.getElementById('portal');
+
+  if (!portalElement) {
+    console.error("Element with id 'portal' not found");
+    return null;
+  }
 
   return ReactDom.createPortal(
     <>
       <div className="modal__overlay"></div>
       <div className="modal">{children}</div>
     </>,
-    document.getElementById('portal'),
+    portalElement,
   );
 }
 
